f215d5b9894c43b5516770868d9de8b6be9a666d
Brojowski <alex.gajowski@gmail.com>
2019-09-06 02:11:46 -0400

    display data using a qrcode

--- src/views/me.vue
+++ src/views/me.vue
@@ -1,25 +1,34 @@
 <template>
     <div>
-        <!-- This is where the QR code will go -->
-        <div id="id-qr"></div>
+        <p></p>
+        <qrcode-vue :value="uId"></qrcode-vue>
     </div>
 </template>
+
+<script>
+import QrcodeVue from 'qrcode.vue'
+
+export default {
+    name: 'Me',
+    components: {
+        QrcodeVue
+    },
+    mounted() {
+        let user = this.$parent.wrapper.userManager.getLocalUser()
+        if (user != null) {
+            // TODO: user.key is not the correct value. Need to get _id
+            this.uId = user.key
+            console.log("set uId", this.uId)
+        }
+    }, 
+    data() {
+        return {
+            uId: ''
+        }
+    }
+}
+</script>
 
 <style>
 
 </style>
-
-<script>
-export default {
-    name: 'Me',
-    mounted() {
-        let user = this.$parent.wrapper.userManager.getLocalUser()
-        if (user != null) {
-            // TODO: Display a qrcode using legacy js
-        }
-    }
-}
-
-
-
-</script>
